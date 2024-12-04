b2bb26f0d548d025b839460c4c369c1d7a86f4c3
Dotinkasra <tyekepon@pm.me>
2024-12-04 09:29:55 +0900

    Add: functionality to change parameters in query

--- main.go
+++ main.go
@@ -43,6 +43,34 @@ type Style struct {
 	Type string `json:"type"`
 }
 
+type AudioQuery struct {
+	AccentPhrases []struct {
+		Moras []struct {
+			Text            string  `json:"text"`
+			Consonant       string  `json:"consonant"`
+			ConsonantLength float64 `json:"consonant_length"`
+			Vowel           string  `json:"vowel"`
+			VowelLength     float64 `json:"vowel_length"`
+			Pitch           float64 `json:"pitch"`
+		} `json:"moras"`
+		Accent          int  `json:"accent"`
+		PauseMora       any  `json:"pause_mora"`
+		IsInterrogative bool `json:"is_interrogative"`
+	} `json:"accent_phrases"`
+	SpeedScale         float64 `json:"speedScale"`
+	IntonationScale    float64 `json:"intonationScale"`
+	TempoDynamicsScale float64 `json:"tempoDynamicsScale"`
+	PitchScale         float64 `json:"pitchScale"`
+	VolumeScale        float64 `json:"volumeScale"`
+	PrePhonemeLength   float64 `json:"prePhonemeLength"`
+	PostPhonemeLength  float64 `json:"postPhonemeLength"`
+	PauseLength        any     `json:"pauseLength"`
+	PauseLengthScale   float64 `json:"pauseLengthScale"`
+	OutputSamplingRate int     `json:"outputSamplingRate"`
+	OutputStereo       bool    `json:"outputStereo"`
+	Kana               string  `json:"kana"`
+}
+
 func (e *Engine) getHost() string {
 	//return net.JoinHostPort(e.Host, strconv.Itoa(e.Port))
 	return e.Host + ":" + strconv.Itoa(e.Port)
@@ -69,7 +97,7 @@ func (e *Engine) getSpeakers() []Speaker {
 	return speakers
 }
 
-func (s *Style) getAudioQuery(host string, text string) json.RawMessage {
+func (s *Style) getAudioQuery(host string, text string) AudioQuery {
 	uri_param := url.Values{}
 	uri_param.Set("text", text)
 	uri_param.Set("speaker", strconv.Itoa(s.Id))
@@ -80,7 +108,6 @@ func (s *Style) getAudioQuery(host string, text string) json.RawMessage {
 	}
 
 	endpoint := uri + "?" + uri_param.Encode()
-	fmt.Println(uri)
 
 	resp, err := http.Post(endpoint, "application/json", nil)
 	if err != nil {
@@ -93,7 +120,7 @@ func (s *Style) getAudioQuery(host string, text string) json.RawMessage {
 		panic(err)
 	}
 
-	var jsonData json.RawMessage
+	var jsonData AudioQuery
 	if err := json.Unmarshal(body, &jsonData); err != nil {
 		fmt.Printf("error decoding sakura response: %v\n", err)
 		if e, ok := err.(*json.SyntaxError); ok {
@@ -105,14 +132,15 @@ func (s *Style) getAudioQuery(host string, text string) json.RawMessage {
 	return jsonData
 }
 
-func (e *Engine) getAudio(audio_query json.RawMessage) {
+func (s *Style) getAudio(host string, query AudioQuery) {
+	jsonQuery, _ := json.Marshal(query)
 	uri_param := url.Values{}
-	uri_param.Set("speaker", "888753765")
+	uri_param.Set("speaker", strconv.Itoa(s.Id))
 
-	uri, _ := url.JoinPath(e.getHost(), "synthesis")
+	uri, _ := url.JoinPath(host, "synthesis")
 	endpoint := uri + "?" + uri_param.Encode()
 
-	resp, _ := http.Post(endpoint, "application/json", bytes.NewBuffer(audio_query))
+	resp, _ := http.Post(endpoint, "application/json", bytes.NewBuffer(jsonQuery))
 	defer resp.Body.Close()
 
 	body, err := io.ReadAll(resp.Body)
@@ -133,18 +161,6 @@ func (e *Engine) getAudio(audio_query json.RawMessage) {
 	<-done
 }
 
-func showSpeakers(e *Engine) {
-	for i, s := range e.getSpeakers() {
-		color.Red(fmt.Sprintf("%d: %s\n", i, s.Name))
-		for j, style := range s.Styles {
-			color.Green(fmt.Sprintf("  %d: %d: %s\n", j, style.Id, style.Name))
-			if style.Id == 888753765 {
-				audio_query := style.getAudioQuery(e.getHost(), "こんにちは")
-				e.getAudio(audio_query)
-			}
-		}
-	}
-}
 func main() {
 	var host string
 	var port int
@@ -174,8 +190,98 @@ func main() {
 			Aliases: []string{"l"},
 			Usage:   "Show speakers",
 			Action: func(c *cli.Context) error {
-				engine := Engine{Host: host, Port: port}
-				showSpeakers(&engine)
+				e := Engine{Host: host, Port: port}
+				for i, s := range e.getSpeakers() {
+					color.Red(fmt.Sprintf("%d: %s\n", i, s.Name))
+					for j, style := range s.Styles {
+						color.Green(fmt.Sprintf("  %d: %d: %s\n", j, style.Id, style.Name))
+					}
+				}
+				return nil
+			},
+		},
+		{
+			Name:    "say",
+			Aliases: []string{"s"},
+			Usage:   "Say something",
+			Flags: []cli.Flag{
+				&cli.IntFlag{
+					Name:     "id",
+					Usage:    "Speaker ID",
+					Required: false,
+				},
+				&cli.IntFlag{
+					Name:     "speaker-number",
+					Aliases:  []string{"sn"},
+					Usage:    "Speaker Number",
+					Required: false,
+				},
+				&cli.IntFlag{
+					Name:     "style-number",
+					Aliases:  []string{"style"},
+					Usage:    "Text to say",
+					Required: false,
+				},
+				&cli.IntFlag{
+					Name:     "accent",
+					Usage:    "Text to say",
+					Value:    -1,
+					Required: false,
+				},
+				&cli.Float64Flag{
+					Name:     "speed",
+					Usage:    "Text to say",
+					Value:    1.0,
+					Required: false,
+				},
+				&cli.Float64Flag{
+					Name:     "intonation",
+					Usage:    "Text to say",
+					Value:    1.0,
+					Required: false,
+				},
+				&cli.Float64Flag{
+					Name:     "tempo",
+					Usage:    "Text to say",
+					Value:    1.0,
+					Required: false,
+				},
+				&cli.Float64Flag{
+					Name:     "pitch",
+					Usage:    "Text to say",
+					Value:    0.0,
+					Required: false,
+				},
+			},
+			Action: func(c *cli.Context) error {
+				e := Engine{Host: host, Port: port}
+				speakers := e.getSpeakers()
+				var style Style
+				if c.Int("id") == 0 {
+					sp := speakers[c.Int("speaker-number")]
+					st := sp.Styles[c.Int("style-number")]
+					style = st
+
+				} else {
+					for _, sp := range speakers {
+						for _, st := range sp.Styles {
+							if st.Id == c.Int("id") {
+								style = st
+							}
+						}
+					}
+				}
+
+				query := style.getAudioQuery(e.getHost(), c.Args().First())
+				query.SpeedScale = c.Float64("speed")
+				query.IntonationScale = c.Float64("intonation")
+				query.TempoDynamicsScale = c.Float64("tempo")
+				query.PitchScale = c.Float64("pitch")
+				if c.Int("accent") != -1 {
+					query.AccentPhrases[0].Accent = c.Int("accent")
+				}
+				style.getAudio(e.getHost(), query)
+
 				return nil
 			},
 		},
