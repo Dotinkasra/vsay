49e8f78f1bbb9889ac23bd93abc4bb29b5afb260
Dotinkasra <tyekepon@pm.me>
2024-12-04 11:06:48 +0900

    add: Quiet option

--- cmd/vsay/main.go
+++ cmd/vsay/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"log"
 	"os"
+	"vsay/pkg/audio"
 	"vsay/pkg/engine"
 	"vsay/pkg/engine/speaker"
 	"vsay/pkg/vsay"
@@ -53,55 +54,66 @@ func main() {
 			Flags: []cli.Flag{
 				&cli.IntFlag{
 					Name:     "id",
-					Usage:    "Speaker ID",
+					Usage:    "Style `ID`. This takes priority over the speaker number option.",
 					Required: false,
 				},
 				&cli.IntFlag{
-					Name:     "speaker-number",
-					Aliases:  []string{"sn"},
-					Usage:    "Speaker Number",
+					Name:     "number",
+					Usage:    "The speaker number as displayed by the `ls` command.",
 					Required: false,
 				},
 				&cli.IntFlag{
-					Name:     "style-number",
-					Aliases:  []string{"style"},
-					Usage:    "Text to say",
+					Name:     "style",
+					Usage:    "The style number as displayed by the `ls` command.",
 					Required: false,
 				},
 				&cli.IntFlag{
 					Name:     "accent",
-					Usage:    "Text to say",
+					Usage:    "Specify the accent by its `index` in the string.",
 					Value:    -1,
 					Required: false,
 				},
 				&cli.Float64Flag{
 					Name:     "speed",
-					Usage:    "Text to say",
+					Usage:    "Set the speaking speed. Valid range: `0.5 to 2.0`.",
 					Value:    1.0,
 					Required: false,
 				},
 				&cli.Float64Flag{
 					Name:     "intonation",
-					Usage:    "Text to say",
+					Usage:    "Set the intonation, affecting the style's strength. Valid range: `0.0 to 2.0`.",
 					Value:    1.0,
 					Required: false,
 				},
 				&cli.Float64Flag{
 					Name:     "tempo",
-					Usage:    "Text to say",
+					Usage:    "Set the tempo. Valid range: `0.0 to 2.0`.",
 					Value:    1.0,
 					Required: false,
 				},
 				&cli.Float64Flag{
 					Name:     "pitch",
-					Usage:    "Text to say",
+					Usage:    "Set the pitch. Valid range: `-0.15 to 0.15`.",
 					Value:    0.0,
+					Required: false,
+				},
+				&cli.StringFlag{
+					Name:     "save",
+					Aliases:  []string{"s"},
+					Usage:    "Specify the `PATH` to save the audio file.",
+					Value:    "",
+					Required: true,
+				},
+				&cli.BoolFlag{
+					Name:     "quiet",
+					Aliases:  []string{"q"},
+					Usage:    "Don't play audio.",
 					Required: false,
 				},
 			},
 			Action: func(c *cli.Context) error {
 				e := engine.Engine{Host: host, Port: port}
-				speakers := e.GetSpeakers()
+				speakers := e.ShowSpeakers()
 				var style speaker.Style
 				if c.Int("id") == 0 {
 					sp := speakers[c.Int("speaker-number")]
@@ -118,7 +130,7 @@ func main() {
 					}
 				}
 
-				query := style.GetAudioQuery(e.GetHost(), c.Args().First())
+				query := style.CreateAudioQuery(e.MyHost(), c.Args().First())
 				query.SpeedScale = c.Float64("speed")
 				query.IntonationScale = c.Float64("intonation")
 				query.TempoDynamicsScale = c.Float64("tempo")
@@ -126,8 +138,13 @@ func main() {
 				if c.Int("accent") != -1 {
 					query.AccentPhrases[0].Accent = c.Int("accent")
 				}
-				style.GetAudio(e.GetHost(), query)
-
+				raw_audio := style.GetAudio(e.MyHost(), query)
+				if !c.Bool("quiet") {
+					audio.PlayAudio(raw_audio)
+				}
+				if c.String("save") != "" {
+					audio.SaveAudio(raw_audio, c.String("save"))
+				}
 				return nil
 			},
 		},
